dcf3fecc0a2168c4bd7b63ed0e1f126845b2bad6
TrvMarco <trevisanello.marco@gmail.com>
2022-08-25 18:15:08 +0200

    add orders table

---
+++ database/migrations/2022_08_25_160319_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('status')->default(false);
+		    $table->float('total_price');
+		    $table->string('client_name', 100);
+		    $table->string('client_surname', 100);
+		    $table->string('address', 200);
+		    $table->string('phone', 80); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
