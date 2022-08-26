081af7b00d93ac2ff7710555ce92d2900b42b573
GiuseppeTodisco92 <giuseppe24todisco92@gmail.com>
2022-08-26 09:21:19 +0200

    add admin-homeblade

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,12 @@ Route::get('/', function () {
 });
 
 Auth::routes();
+Route::middleware('auth')
+    ->namespace('Admin')
+    ->name('admin.')
+    ->prefix('admin')
+    ->group(function(){
+        Route::get('/home', 'HomeController@index')->name('home');
+    });
 
-Route::get('/home', 'HomeController@index')->name('home');
+
