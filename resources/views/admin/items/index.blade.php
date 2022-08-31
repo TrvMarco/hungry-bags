a32c9e72e4607a12d00710203b604739a729a5d9
TrvMarco <trevisanello.marco@gmail.com>
2022-08-31 17:24:59 +0200

    Implementazione tabella items

--- resources/views/admin/items/index.blade.php
+++ resources/views/admin/items/index.blade.php
@@ -2,47 +2,56 @@
 
 @section('content')
     <div class="container">
-        <h1>Il tuo Menù</h1>
+        <div class="row mb-4 justify-content-center">
+          <h1>Il tuo Menù</h1>
             @if ($items->isEmpty())
             <h1>il tuo menu è vuoto </h1>
             <a href="{{route('admin.items.create')}}">Inserisci un nuovo piatto</a>
             <br>
             <a href="{{route('admin.home')}}">Torna alla pagina Home</a>
-        @endif
-        <ul>
-            @foreach($items as $item)
-            <table class="table">
-                <thead>
-                  <tr>
-                    <th scope="col">Foto del piatto</th>
-                    <th scope="col">Nome</th>
-                    <th scope="col">Azioni</th>
+            @endif
+        </div>
+        <div class="row">
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">Foto del piatto</th>
+                <th scope="col">Nome del piatto</th>
+                <th scope="col">Visibile nel Menù</th>
+                <th scope="col">Azioni</th>
+              </tr>
+            </thead>
+            <tbody>
+              @foreach($items as $item)
+              <tr>
+                    <th scope="row">
+                      <img class="img-fluid menu_item_img" src="{{ asset('storage/' . $item->image)}}" alt="{{$item->name}}">
+                    </th>
+                    <td>{{$item->name}}</td>
+                    <td class="d-flex justify-content-center">
+                      @if ($item->is_visible)
+                        <div class="menu_eye_green"><i class="fa-solid fa-eye"></i></div>
+                      @else
+                        <div class="menu_eye_red"><i class="fa-solid fa-eye-slash"></i></div>
+                      @endif
+                    </td>
+                    <td>
+                        <div class="d-flex">
+                            <a href="{{route('admin.items.show', $item->id)}}" class="btn btn-primary mx-2 btn-sm">Visualizza</a>
+                            <a href="{{route('admin.items.edit', $item->id)}}" class="btn btn-warning mx-2 btn-sm">Modifica</a>
+                             {{-- bottone elimina --}}
+                            <form action="{{route('admin.items.destroy', $item->id)}}" method="POST">
+                              @csrf
+                              @method('DELETE')
+                              <button type="submit" class="btn btn-danger btn-sm">Cancella</button>
+                            </form>
+                        </div>
+                    </td>
                   </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">3</th>
-                    <td>Larry</td>
-                    <td>the Bird</td>
-                    <td>@twitter</td>
-                  </tr>
+                  @endforeach
                 </tbody>
-              </table>
-            @endforeach
-        </ul>
-        
+            </table>
+        </div>
     </div>
     @endsection
     {{-- <li class="d-flex my-3 justify-content-between"> --}}
