e156845e5c0639a28c66036b61ffdbb42851881d
Ferlo94 <100360746+Ferlo94@users.noreply.github.com>
2022-08-31 16:22:14 +0200

    Aggiunti bottoni ACCEDI REGISTRATI ad Area Ristoratori

--- resources/js/pages/RestaurantArea.vue
+++ resources/js/pages/RestaurantArea.vue
@@ -1,13 +1,63 @@
 <template>
-  <h1>Questa e' l'area ristoratori</h1>
+  
+  <div class="main">
+
+
+      <div class="container accedi">
+        <a class="link" href="/login">LOGIN</a>
+      </div>
+
+
+      <div class="container registrati">
+        <a class="link" href="/register">REGISTRATI</a>
+      </div>
+
+  </div>
+
 </template>
 
 <script>
 export default {
-    name: 'RestaurantArea'
+  name: 'RestaurantArea'
 }
 </script>
 
-<style>
+<style lang='scss' scoped>
+
+  .main {
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    .container{
+      background: var(--primary-color);
+    }
+
+    .accedi, .registrati {
+      margin: 10px;
+      height: 40px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      
+    }
+
+    .accedi {
+      margin-top: 30px;
+    }
+
+    .registrati {
+      background: var(--secondary-color);
+      
+    }
+
+    .link {
+      text-decoration: none;
+      color: inherit;
+    }
+  }
 
 </style>
