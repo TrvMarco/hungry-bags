90a050893fcf49c59a505870808c47ff8e434900
GiuseppeTodisco92 <giuseppe24todisco92@gmail.com>
2022-08-31 21:54:49 +0200

    add placeholder image user

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -7,7 +7,13 @@
             <div class="col-4">
                 {{-- CARD RIEPILOGO DATI RISTORANTE --}}
                 <div class="card">
-                    <img class="card-img-top" src="{{ asset('storage/' . $user->image)}}" alt="Card image cap">
+                    @if ($user->image)
+                        <img class="card-img-top" src="{{ asset('storage/' . $user->image)}}" alt="Card image cap">
+                    @else
+                        <a href="{{route('admin.users.edit', $user)}}"><img src="https://placehold.jp/150x150.png" alt=""></a>
+                        <small>clicca per inserire immagine</small>
+                    @endif
+                    
                     <div class="card-body">
                     <h5 class="card-title"><strong>{{ $user->name }}</strong></h5>
                     <p class="card-text"><strong>Indirizzo:</strong> {{ $user->address}}</p>
