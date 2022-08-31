3bcfd05538c52141b53b3db79fd79e3588fe2ec0
TrvMarco <trevisanello.marco@gmail.com>
2022-08-31 18:03:15 +0200

    file strano

--- public/js/app.js
+++ public/js/app.js
@@ -49646,6 +49646,17 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./resources/sass/back.scss":
+/*!**********************************!*\
+  !*** ./resources/sass/back.scss ***!
+  \**********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nError: ENOENT: no such file or directory, open 'C:\\Progetti Boolean\\hungry-bags\\resources\\sass\\back.scss'\n    at C:\\Progetti Boolean\\hungry-bags\\node_modules\\webpack\\lib\\NormalModule.js:316:20\n    at C:\\Progetti Boolean\\hungry-bags\\node_modules\\loader-runner\\lib\\LoaderRunner.js:367:11\n    at C:\\Progetti Boolean\\hungry-bags\\node_modules\\loader-runner\\lib\\LoaderRunner.js:203:19\n    at C:\\Progetti Boolean\\hungry-bags\\node_modules\\enhanced-resolve\\lib\\CachedInputFileSystem.js:85:15\n    at processTicksAndRejections (node:internal/process/task_queues:78:11)");
+
+/***/ }),
+
 /***/ "./resources/sass/front.scss":
 /*!***********************************!*\
   !*** ./resources/sass/front.scss ***!
@@ -49658,15 +49669,16 @@ __webpack_require__.r(__webpack_exports__);
 /***/ }),
 
 /***/ 0:
-/*!*****************************************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/front.scss ***!
-  \*****************************************************************************************/
+/*!********************************************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/front.scss ./resources/sass/back.scss ***!
+  \********************************************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\lafer\OneDrive\Desktop\Progetto finale\hungry-bags\hungry-bags\resources\js\app.js */"./resources/js/app.js");
-__webpack_require__(/*! C:\Users\lafer\OneDrive\Desktop\Progetto finale\hungry-bags\hungry-bags\resources\sass\app.scss */"./resources/sass/app.scss");
-module.exports = __webpack_require__(/*! C:\Users\lafer\OneDrive\Desktop\Progetto finale\hungry-bags\hungry-bags\resources\sass\front.scss */"./resources/sass/front.scss");
+__webpack_require__(/*! C:\Progetti Boolean\hungry-bags\resources\js\app.js */"./resources/js/app.js");
+__webpack_require__(/*! C:\Progetti Boolean\hungry-bags\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Progetti Boolean\hungry-bags\resources\sass\front.scss */"./resources/sass/front.scss");
+module.exports = __webpack_require__(/*! C:\Progetti Boolean\hungry-bags\resources\sass\back.scss */"./resources/sass/back.scss");
 
 
 /***/ })
