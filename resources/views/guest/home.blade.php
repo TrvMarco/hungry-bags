600afc96526447eefba10e33686d353f668bf935
kritoma <toma.kristian97@gmail.com>
2022-08-31 10:59:57 +0200

    fix scss

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Front Office</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="{{asset('css/front.css')}}">
 </head>
 <body>
     {{-- popolato da VUE --}}
