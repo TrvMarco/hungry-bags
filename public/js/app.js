454cc88e6f43c516f7680c17ce841d0b7abb0806
Ferlo94 <100360746+Ferlo94@users.noreply.github.com>
2022-08-31 09:56:43 +0200

    aggiunti header main e footer vue, installato vue router e create HOME vue ROUTER js (Lezione 91A min 36)

--- public/js/app.js
+++ public/js/app.js
@@ -49664,9 +49664,9 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Giuseppe\Desktop\BOOLEAN\ESERCIZI\PHP\hungry-bags\resources\js\app.js */"./resources/js/app.js");
-__webpack_require__(/*! C:\Users\Giuseppe\Desktop\BOOLEAN\ESERCIZI\PHP\hungry-bags\resources\sass\app.scss */"./resources/sass/app.scss");
-module.exports = __webpack_require__(/*! C:\Users\Giuseppe\Desktop\BOOLEAN\ESERCIZI\PHP\hungry-bags\resources\sass\front.scss */"./resources/sass/front.scss");
+__webpack_require__(/*! C:\Users\lafer\OneDrive\Desktop\Progetto finale\hungry-bags\hungry-bags\resources\js\app.js */"./resources/js/app.js");
+__webpack_require__(/*! C:\Users\lafer\OneDrive\Desktop\Progetto finale\hungry-bags\hungry-bags\resources\sass\app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! C:\Users\lafer\OneDrive\Desktop\Progetto finale\hungry-bags\hungry-bags\resources\sass\front.scss */"./resources/sass/front.scss");
 
 
 /***/ })
