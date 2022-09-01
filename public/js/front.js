b5c75e8c2ef2174393ff0497f81388baeb6ac81b
kritoma <toma.kristian97@gmail.com>
2022-09-01 18:07:42 +0200

    commit fix

--- public/js/front.js
+++ public/js/front.js
@@ -19715,7 +19715,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\Giuseppe\Desktop\BOOLEAN\ESERCIZI\PHP\hungry-bags\resources\js\front.js */"./resources/js/front.js");
+module.exports = __webpack_require__(/*! C:\Users\valen\Desktop\hungry-bags\resources\js\front.js */"./resources/js/front.js");
 
 
 /***/ })
