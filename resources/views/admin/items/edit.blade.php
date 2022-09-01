da3de2e64f46c1976069c29080eb9a5301a595dd
TrvMarco <trevisanello.marco@gmail.com>
2022-09-01 11:14:43 +0200

    modifica index e edit items

--- resources/views/admin/items/edit.blade.php
+++ resources/views/admin/items/edit.blade.php
@@ -1,83 +1,82 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-
-    <form action="{{ route('admin.items.update', $item->id) }}" method="POST" enctype="multipart/form-data">
-        @csrf
-        @method('PUT')
-    
-        {{-- NOME PIATTO --}}
-        <div class="row">
-            <div class="col-5">
-                <div class="form-group">
-                    <label for="name">Nome: *</label>
-                    <input id="name" type="text" maxlength="100" class="form-control @error('name') is-invalid @enderror"  name="name" placeholder="Inserisci il nome del piatto" value="{{old('name', $item->name)}}" required autocomplete="name" autofocus>
-                    @error('name')
-                        <div class="alert alert-danger">{{ $message }}</div>
-                    @enderror
+    <div class="container">
+            <form class="bg-white p-4" action="{{ route('admin.items.update', $item->id) }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                @method('PUT')
+            
+                {{-- NOME PIATTO --}}
+                <div class="row">
+                    <div class="col-5">
+                        <div class="form-group">
+                            <label for="name">Nome: *</label>
+                            <input id="name" type="text" maxlength="100" class="form-control @error('name') is-invalid @enderror"  name="name" placeholder="Inserisci il nome del piatto" value="{{old('name', $item->name)}}" required autocomplete="name" autofocus>
+                            @error('name')
+                                <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-
-        {{-- DESCRIZIONE --}}
-        <div class="row">
-            <div class="col-8">
-                <div class="form-group">
-                    <label for="description">Descizione: *</label>
-                    <textarea class="form-control  @error('description') is-invalid @enderror" name="description" id="description" placeholder="Inserisci una descrizione" cols="30" rows="5" name="description" required autocomplete="description" autofocus>{{old('description', $item->description)}}</textarea>
-                    @error('description')
+        
+                {{-- DESCRIZIONE --}}
+                <div class="row">
+                    <div class="col-8">
+                        <div class="form-group">
+                            <label for="description">Descizione: *</label>
+                            <textarea class="form-control  @error('description') is-invalid @enderror" name="description" id="description" placeholder="Inserisci una descrizione" cols="30" rows="5" name="description" required autocomplete="description" autofocus>{{old('description', $item->description)}}</textarea>
+                            @error('description')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+                    </div>
+                </div>
+        
+                {{-- PREZZO --}}
+                <div class="row">
+                    <div class="col-3">
+                        <div class="form-group">
+                            <label for="price">Prezzo: *</label>
+                            <div class="input-group-prepend">
+                                <span class="input-group-text">&#x20AC;</span>
+                                <input class="form-control input-group-append @error('price') is-invalid @enderror" type="number" min="0.00" max="200" placeholder="Inserisci il prezzo" step="0.1" name="price" value="{{old('price', $item->price)}}" required autocomplete="price" autofocus/>
+                            </div>
+                            @error('price')
+                                <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+                    </div>
+                </div>
+        
+                {{-- IMMAGINE --}}
+                <div class="form-group mb-4">
+                    <label for="image">Immagine:</label>
+                    @if ($item->image)
+                    <div>
+                        <img src="{{asset("storage/{$item->image}")}}" alt="immagine">
+                    </div>
+                @endif
+                    <input type="file" accept=".png,.jpeg,.jpg" class="form-control-file @error('image') is-invalid @enderror" id="image" name="image" value="{{old('image')}}">
+                    @error('image')
                     <div class="alert alert-danger">{{ $message }}</div>
                     @enderror
                 </div>
-            </div>
-        </div>
-
-        {{-- PREZZO --}}
-        <div class="row">
-            <div class="col-3">
-                <div class="form-group">
-                    <label for="price">Prezzo: *</label>
-                    <div class="input-group-prepend">
-                        <span class="input-group-text">&#x20AC;</span>
-                        <input class="form-control input-group-append @error('price') is-invalid @enderror" type="number" min="0.00" max="200" placeholder="Inserisci il prezzo" step="0.1" name="price" value="{{old('price', $item->price)}}" required autocomplete="price" autofocus/>
-                    </div>
-                    @error('price')
-                        <div class="alert alert-danger">{{ $message }}</div>
+                
+                {{-- PUBBLICARLO NEL MENÙ --}}
+                <div class="form-check mb-3">
+                    <input type="checkbox" class="form-check-input @error('is_visible') is-invalid @enderror" id="is_visible" name="is_visible" {{old('is_visible', $item->is_visible) ? 'checked' : ''}}>
+                    <label class="form-check-label" for="is_visible">Pubblica il piatto nel menù</label>
+                    @error('is_visible')
+                    <div class="alert alert-danger">{{ $message }}</div>
                     @enderror
                 </div>
-            </div>
-        </div>
-
-        {{-- IMMAGINE --}}
-        <div class="form-group mb-4">
-            <label for="image">Immagine:</label>
-            @if ($item->image)
-            <div>
-                <img src="{{asset("storage/{$item->image}")}}" alt="immagine">
-            </div>
-        @endif
-            <input type="file" accept=".png,.jpeg,.jpg" class="form-control-file @error('image') is-invalid @enderror" id="image" name="image" value="{{old('image')}}">
-            @error('image')
-            <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
-        </div>
+                
+                <hr>
         
-        {{-- PUBBLICARLO NEL MENÙ --}}
-        <div class="form-check mb-3">
-            <input type="checkbox" class="form-check-input @error('is_visible') is-invalid @enderror" id="is_visible" name="is_visible" {{old('is_visible', $item->is_visible) ? 'checked' : ''}}>
-            <label class="form-check-label" for="is_visible">Pubblica il piatto nel menù</label>
-            @error('is_visible')
-            <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
-        </div>
+                <div class="mb-3"><small>I campi contrassegnati con * sono obbligatori</small></div>
         
-        <hr>
-
-        <div class="mb-3"><small>I campi contrassegnati con * sono obbligatori</small></div>
-
-        <button type="submit" class="btn btn-warning">Modifica piatto</button>
-        
-    </form>
-</div>
+                <button type="submit" class="btn btn-warning">Modifica piatto</button>
+                
+            </form>
+    </div>
 @endsection
--- resources/views/admin/items/index.blade.php
+++ resources/views/admin/items/index.blade.php
@@ -2,68 +2,73 @@
 
 @section('content')
     <div class="container backoffice_menu_container">
-        <div class="row mb-4 justify-content-center pt-4">
+        @if ($items->isEmpty())
+          <div class="row mb-4 pt-4 justify-content-around">
+              <div class="col-12 text-center p-5">
+                <h2>Il tuo Menù è vuoto!</h2>
+              </div>
+              <div class="item_menu_box">
+                <a href="{{route('admin.items.create')}}"><i class="fa-solid fa-plus"></i></a>
+                <span>Inserisci un piatto</span>
+              </div>
+              <div class="item_menu_box"><a href="{{route('admin.home')}}">Torna alla pagina Home</a></div>
+          </div>
+          @else
           <h1>Il tuo Menù</h1>
-            @if ($items->isEmpty())
-            <h1>il tuo menu è vuoto </h1>
-            <a href="{{route('admin.items.create')}}">Inserisci un nuovo piatto</a>
-            <br>
-            <a href="{{route('admin.home')}}">Torna alla pagina Home</a>
-            @endif
-        </div>
-        <div class="row">
-          <table class="table">
-            <thead>
-              <tr>
-                <th scope="col">Foto del piatto</th>
-                <th scope="col">Nome del piatto</th>
-                <th scope="col" class="text-center">Visibile nel Menù</th>
-                <th scope="col" class="text-center">Azioni</th>
+          <div class="row">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">Foto del piatto</th>
+                  <th scope="col">Nome del piatto</th>
+                  <th scope="col" class="text-center">Visibile nel Menù</th>
+                  <th scope="col" class="text-center">Azioni</th>
               </tr>
             </thead>
             <tbody>
               @foreach($items as $item)
               <tr>
-                    <th scope="row">
-                      @if ($item->image)
-                      <img class="img-fluid menu_item_img" src="{{ asset('storage/' . $item->image)}}" alt="{{$item->name}}">
-                      @else
-                          <p>null</p>
-                      @endif
-                      
-                    </th>
-                    <td>{{$item->name}}</td>
-                    <td class="text-center">
-                      @if ($item->is_visible)
-                        <div class="menu_eye_green"><i class="fa-solid fa-eye"></i></div>
-                      @else
-                        <div class="menu_eye_red"><i class="fa-solid fa-eye-slash"></i></div>
-                      @endif
-                    </td>
-                    <td>
-                        <div class="d-flex justify-content-center">
-                            <a href="{{route('admin.items.show', $item->id)}}" class="btn btn-primary mx-2 btn-sm">Visualizza</a>
-                            <a href="{{route('admin.items.edit', $item->id)}}" class="btn btn-warning mx-2 btn-sm">Modifica</a>
-                             {{-- bottone elimina --}}
-                            <form action="{{route('admin.items.destroy', $item->id)}}" method="POST">
-                              @csrf
-                              @method('DELETE')
-                              <button type="submit" class="btn btn-danger btn-sm">Cancella</button>
-                            </form>
-                        </div>
-                    </td>
-                  </tr>
-                  @endforeach
-                </tbody>
-            </table>
+                <th scope="row">
+                  @if ($item->image)
+                  <img class="img-fluid menu_item_img" src="{{ asset('storage/' . $item->image)}}" alt="{{$item->name}}">
+                  @else
+                  <p>null</p>
+                  @endif
+                  
+                </th>
+                <td>{{$item->name}}</td>
+                <td class="text-center">
+                  @if ($item->is_visible)
+                  <div class="menu_eye_green"><i class="fa-solid fa-eye"></i></div>
+                  @else
+                  <div class="menu_eye_red"><i class="fa-solid fa-eye-slash"></i></div>
+                  @endif
+                </td>
+                <td>
+                  <div class="d-flex justify-content-center">
+                    <a href="{{route('admin.items.show', $item->id)}}" class="btn btn-primary mx-2 btn-sm">Visualizza</a>
+                    <a href="{{route('admin.items.edit', $item->id)}}" class="btn btn-warning mx-2 btn-sm">Modifica</a>
+                    {{-- bottone elimina --}}
+                    <form action="{{route('admin.items.destroy', $item->id)}}" method="POST">
+                      @csrf
+                      @method('DELETE')
+                      <button type="submit" class="btn btn-danger btn-sm">Cancella</button>
+                    </form>
+                  </div>
+                </td>
+              </tr>
+              @endforeach
+            </tbody>
+          </table>
         </div>
-    </div>
-    @endsection
-    {{-- <li class="d-flex my-3 justify-content-between"> --}}
+        @endif
+      </div>
+      @endsection
+      {{-- <li class="d-flex my-3 justify-content-between"> --}}
         {{-- bottone modifica --}}
         {{-- {{$item->name}}
         <div class="d-flex">
-            <a href="{{route('admin.items.show', $item->id)}}" class="btn btn-primary mx-2">Visualizza</a>
+          <a href="{{route('admin.items.show', $item->id)}}" class="btn btn-primary mx-2">Visualizza</a>
             <a href="{{route('admin.items.edit', $item->id)}}" class="btn btn-warning mx-2">Modifica</a> --}}
             {{-- bottone elimina --}}
             {{-- <form action="{{route('admin.items.destroy', $item->id)}}" method="POST">
