aac292be992b4cb599c9e9401d8789c6e490ebec
kritoma <toma.kristian97@gmail.com>
2022-09-02 15:54:22 +0200

    add api for user list

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,9 +3,19 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Type;
 use Illuminate\Http\Request;
+use App\User;
+
+
+use function PHPSTORM_META\type;
 
 class UserController extends Controller
-{
-
+{   
+    public function index(Request $request)
+    {
+        $data = $request->all();
+        $user = Type::where('name', $data)->with(['users'])->get();
+        return $user;
+    }
 }
