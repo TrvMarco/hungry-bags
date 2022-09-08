219ef6fb221fd213d779204b408b300b95f64739
Ferlo94 <100360746+Ferlo94@users.noreply.github.com>
2022-09-08 10:39:54 +0200

    register colori sobri

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+                <div class="card-header beige-bg">{{ __('Register') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}" enctype="multipart/form-data">
@@ -137,7 +137,7 @@
                         {{-- REGISTER BUTTON --}}
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-primary black-btn">
                                     {{ __('Register') }}
                                 </button>
                             </div>
